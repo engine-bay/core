4469db10c9e977a8bd2dd32b68e8fad43d7cb762
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-17 14:28:39 +0000

    Added query/command handler Handle interfaces definitions

--- EngineBay.Core/Interfaces/ICommandHandler.cs
+++ EngineBay.Core/Interfaces/ICommandHandler.cs
@@ -2,5 +2,6 @@ namespace EngineBay.Core
 {
     public interface ICommandHandler<TInputParameters, TOutputDto>
     {
+        public Task<TOutputDto> Handle(TInputParameters inputParameters, CancellationToken cancellation);
     }
 }
--- EngineBay.Core/Interfaces/IQueryHandler.cs
+++ EngineBay.Core/Interfaces/IQueryHandler.cs
@@ -2,5 +2,6 @@ namespace EngineBay.Core
 {
     public interface IQueryHandler<TQueryParameters, TOutputDto>
     {
+        public Task<TOutputDto> Handle(TQueryParameters queryParameters, CancellationToken cancellation);
     }
 }
