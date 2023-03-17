0fc3f9d31fbf90620ad0ddbd0a33315322f7a401
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-17 15:15:07 +0000

    Changed module extensions to be callable by the currently running assembly.

--- EngineBay.Core/Interfaces/IModule.cs
+++ EngineBay.Core/Interfaces/IModule.cs
@@ -6,40 +6,4 @@ namespace EngineBay.Core
 
         IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
     }
-
-    public static class ModuleExtensions
-    {
-        private static readonly List<IModule> RegisteredModules = new List<IModule>();
-
-        public static IServiceCollection RegisterModules(this IServiceCollection services)
-        {
-            var modules = DiscoverModules();
-            foreach (var module in modules)
-            {
-                module.RegisterModule(services);
-                RegisteredModules.Add(module);
-            }
-
-            return services;
-        }
-
-        public static WebApplication MapEndpoints(this WebApplication app)
-        {
-            foreach (var module in RegisteredModules)
-            {
-                module.MapEndpoints(app);
-            }
-
-            return app;
-        }
-
-        private static IEnumerable<IModule> DiscoverModules()
-        {
-            return typeof(IModule).Assembly
-                .GetTypes()
-                .Where(p => p.IsClass && p.IsAssignableTo(typeof(IModule)))
-                .Select(Activator.CreateInstance)
-                .Cast<IModule>();
-        }
-    }
 }
