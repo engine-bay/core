ce29922a108b40ee086aac1eeeedcb6d2f1a6a8c
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-19 10:16:34 +0000

    Adds IConfiguration to the IModule registration interface

--- EngineBay.Core/Interfaces/IModule.cs
+++ EngineBay.Core/Interfaces/IModule.cs
@@ -2,7 +2,7 @@ namespace EngineBay.Core
 {
     public interface IModule
     {
-        IServiceCollection RegisterModule(IServiceCollection services);
+        IServiceCollection RegisterModule(IServiceCollection services, IConfiguration configuration);
 
         IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
     }
