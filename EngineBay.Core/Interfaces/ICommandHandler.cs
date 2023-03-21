ab82adf690c2b755c403ab7d0a2a6c0f35c713ba
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-21 23:39:53 +0000

    #major added user to command handler interface

--- EngineBay.Core/Interfaces/ICommandHandler.cs
+++ EngineBay.Core/Interfaces/ICommandHandler.cs
@@ -1,7 +1,7 @@
 namespace EngineBay.Core
 {
-    public interface ICommandHandler<TInputParameters, TOutputDto>
+    public interface ICommandHandler<TInputParameters, TUser, TOutputDto>
     {
-        public Task<TOutputDto> Handle(TInputParameters inputParameters, CancellationToken cancellation);
+        public Task<TOutputDto> Handle(TInputParameters inputParameters, TUser user, CancellationToken cancellation);
     }
 }
