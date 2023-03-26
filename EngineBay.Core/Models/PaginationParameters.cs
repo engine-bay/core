986cb784a97c71d04d6f43e6543debeef80d457d
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-26 21:49:45 +0100

    #major simplified pagination parameters

--- EngineBay.Core/Models/PaginationParameters.cs
+++ EngineBay.Core/Models/PaginationParameters.cs
@@ -2,36 +2,20 @@ namespace EngineBay.Core
 {
     public class PaginationParameters
     {
-        private int skipValue;
-
-        private int limitValue;
-
-        private string? sortBy;
-
-        private SortOrderType? sortOrder;
-
-        public int? Skip
+        public PaginationParameters()
         {
-            get { return this.skipValue; }
-            set { this.skipValue = value.GetValueOrDefault(0); }
+            this.Skip = 0;
+            this.Limit = 10;
+            this.SortBy = "LastUpdatedAt";
+            this.SortOrder = SortOrderType.None;
         }
 
-        public int? Limit
-        {
-            get { return this.limitValue; }
-            set { this.limitValue = value.GetValueOrDefault(10); }
-        }
+        public int Skip { get; set; }
 
-        public string? SortBy
-        {
-            get { return this.sortBy; }
-            set { this.sortBy = value is null ? value : "LastUpdatedAt"; } // ordering by last modified as a sensible fallback
-        }
+        public int Limit { get; set; }
 
-        public SortOrderType? SortOrder
-        {
-            get { return this.sortOrder; }
-            set { this.sortOrder = value.GetValueOrDefault(SortOrderType.None); }
-        }
+        public string SortBy { get; set; }
+
+        public SortOrderType SortOrder { get; set; }
     }
 }
