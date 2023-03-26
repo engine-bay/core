ac4265a121a366f3ecfea60d1c1710aa5a9f65ec
Matthew van der Velden <matthew.vdvelden@gmail.com>
2023-03-26 22:40:12 +0100

    #minor added constructors for SearchParameters

--- EngineBay.Core/Models/SearchParameters.cs
+++ EngineBay.Core/Models/SearchParameters.cs
@@ -2,6 +2,17 @@ namespace EngineBay.Core
 {
     public class SearchParameters : PaginationParameters
     {
+        public SearchParameters(string search, int? skip, int? limit, string? sortBy, SortOrderType? sortOrder)
+            : base(skip, limit, sortBy, sortOrder)
+        {
+            this.Search = search;
+        }
+
+        public SearchParameters()
+            : base()
+        {
+        }
+
         public string? Search { get; set; }
     }
 }
