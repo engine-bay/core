1d23ad474a56da9309ded1ad01f56bcb58618952
Matthew van der Velden <matthew.vdvelden@entlect.co.uk>
2023-09-24 08:18:56 +0100

    #minor adds BindAsync for FilterParameters

--- EngineBay.Core/Models/FilterParameters.cs
+++ EngineBay.Core/Models/FilterParameters.cs
@@ -8,5 +8,30 @@ namespace EngineBay.Core
         }
 
         public IEnumerable<Guid>? Ids { get; set; }
+
+        public static ValueTask<FilterParameters> BindAsync(HttpContext context)
+        {
+            var queries = context?.Request.Query[nameof(Ids)].ToList();
+            if (queries is null)
+            {
+                return ValueTask.FromResult(new FilterParameters());
+            }
+
+            var result = new FilterParameters
+            {
+                Ids = queries.Select(id =>
+                {
+                    var parsedId = Guid.Empty;
+                    if (Guid.TryParse(id, out parsedId))
+                    {
+                        return parsedId;
+                    }
+
+                    return Guid.Empty;
+                }).ToList(),
+            };
+
+            return ValueTask.FromResult(result);
+        }
     }
 }
