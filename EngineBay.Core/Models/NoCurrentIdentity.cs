c004d4f2ab68c0d9351988707b7d561d9bcf8156
GreatMar2 <greatmar2@gmail.com>
2023-10-23 08:15:25 +0100

    Unit tests aside from database management module working

--- EngineBay.Core/Models/NoCurrentIdentity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EngineBay.Core
-{
-    public class NoCurrentIdentity : ICurrentIdentity
-    {
-        public string? Username => null;
-
-        public Guid UserId => Guid.Empty;
-    }
-}
