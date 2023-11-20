7c3f45c990b064fa0fdfe4c535c42fed62cc7b13
GreatMar2 <greatmar2@gmail.com>
2023-11-20 14:26:44 +0000

    Clean up old attempts at dynamic filtering

--- EngineBay.Core/Queries/Filter.cs
+++ EngineBay.Core/Queries/Filter.cs
@@ -2,7 +2,6 @@
 {
     using System.Globalization;
     using System.Linq.Expressions;
-    using System.Reflection;
 
     public class Filter
     {
@@ -36,106 +35,10 @@
 
         public string Field { get; set; }
 
-        // public Expression Operation { get; set; }
         public string Operator { get; set; }
 
         public string Value { get; set; }
 
-        // Attempt 1 - probably works but I didn't like the anotation
-        // [RequiresUnreferencedCode("Dynamic Filter")]
-        // public Expression<Func<TModel, bool>> CreateFilterPredicate<TModel>()
-        // {
-        //    var modelParameter = Expression.Parameter(typeof(TModel));
-        //    var property = Expression.Property(modelParameter, this.Field);
-        //    var format = new DateTimeFormatInfo();
-        //    var value = Expression.Constant(Convert.ChangeType(this.Value, property.Type, format));
-
-        // var comparison = this.GetOperatorComparison(property, value);
-
-        // return Expression.Lambda<Func<TModel, bool>>(comparison, modelParameter);
-        // }
-
-        // Attempt 2 - works
-        public Expression<Func<TModel, bool>> CreateFilterPredicate<TModel>(PropertyInfo property)
-        {
-            if (property is null)
-            {
-                throw new ArgumentNullException(nameof(property));
-            }
-
-            var modelParameter = Expression.Parameter(typeof(TModel));
-            var expressionProperty = Expression.Property(modelParameter, property);
-            var format = new DateTimeFormatInfo();
-            var value = Expression.Constant(Convert.ChangeType(this.Value, property.PropertyType, format));
-
-            var comparison = this.GetOperatorComparison(expressionProperty, value);
-
-            return Expression.Lambda<Func<TModel, bool>>(comparison, modelParameter);
-        }
-
-        // Attempt 3 - Does not work
-        // Error: System.InvalidOperationException: Invoke cannot evaluate LambdaExpression from 'value(EngineBay.DemoModule.QueryTodoItem+<>c__DisplayClass2_1).filter.GetOperatorComparison()'. Ensure that your function/property/member returns LambdaExpression
-        public Func<TValue, TValue, bool> GetOperatorComparison<TValue>()
-            where TValue : IComparable<TValue>
-        {
-            return this.Operator.ToLower(CultureInfo.CurrentCulture) switch
-            {
-                "eq" => (a, b) => a.CompareTo(b) == 0,
-                "neq" => (a, b) => a.CompareTo(b) != 0,
-                "gt" => (a, b) => a.CompareTo(b) > 0,
-                "gte" => (a, b) => a.CompareTo(b) >= 0,
-                "lt" => (a, b) => a.CompareTo(b) < 0,
-                "lte" => (a, b) => a.CompareTo(b) <= 0,
-                _ => throw new ArgumentException($"Invalid operator string {this.Operator}."),
-            };
-        }
-
-        // Attempt 4 - Does not work
-        public Expression<Func<TModel, bool>> CreateFilterPredicate<TModel, TValue>(ParameterExpression parameter, Expression<Func<TModel, TValue>> propertySelector)
-            where TValue : IComparable<TValue>
-        {
-            if (parameter is null)
-            {
-                throw new ArgumentNullException(nameof(parameter));
-            }
-
-            if (propertySelector is null)
-            {
-                throw new ArgumentNullException(nameof(propertySelector));
-            }
-
-            var format = new DateTimeFormatInfo();
-            var value = Expression.Constant(Convert.ChangeType(this.Value, typeof(TValue), format));
-
-            var comparison = this.GetOperatorComparison(propertySelector.Body, value);
-
-            return Expression.Lambda<Func<TModel, bool>>(comparison, parameter);
-        }
-
-        // Attempt 4
-        public Expression<Func<TModel, bool>> CreateFilterPredicate<TModel, TValue>(ParameterExpression parameter, Expression<Func<TModel, TValue?>> propertySelector)
-            where TValue : struct, IComparable<TValue>
-        {
-            if (parameter is null)
-            {
-                throw new ArgumentNullException(nameof(parameter));
-            }
-
-            if (propertySelector is null)
-            {
-                throw new ArgumentNullException(nameof(propertySelector));
-            }
-
-            var format = new DateTimeFormatInfo();
-            var value = Expression.Constant(Convert.ChangeType(this.Value, typeof(TValue), format));
-
-            var comparison = this.GetOperatorComparison(propertySelector.Body, value);
-
-            return Expression.Lambda<Func<TModel, bool>>(comparison, parameter);
-        }
-
-        // Attempt 5 - Works
-        // Needed some rework on the type conversion
         public Expression<Func<TModel, bool>> CreateFilterPredicate<TModel>(ParameterExpression parameter, MemberExpression property, ConstantExpression constant)
         {
             if (parameter is null)
@@ -158,23 +61,18 @@
                 constant = Expression.Constant(constant.Value, property.Type);
             }
 
-            var comparison = this.GetOperatorComparison(property, constant);
-
-            return Expression.Lambda<Func<TModel, bool>>(comparison, parameter);
-        }
-
-        private BinaryExpression GetOperatorComparison(Expression left, Expression right)
-        {
-            return this.Operator.ToLower(CultureInfo.CurrentCulture) switch
+            var comparison = this.Operator.ToLower(CultureInfo.CurrentCulture) switch
             {
-                "eq" => Expression.Equal(left, right),
-                "neq" => Expression.NotEqual(left, right),
-                "gt" => Expression.GreaterThan(left, right),
-                "gte" => Expression.GreaterThanOrEqual(left, right),
-                "lt" => Expression.LessThan(left, right),
-                "lte" => Expression.LessThanOrEqual(left, right),
+                "eq" => Expression.Equal(property, constant),
+                "neq" => Expression.NotEqual(property, constant),
+                "gt" => Expression.GreaterThan(property, constant),
+                "gte" => Expression.GreaterThanOrEqual(property, constant),
+                "lt" => Expression.LessThan(property, constant),
+                "lte" => Expression.LessThanOrEqual(property, constant),
                 _ => throw new ArgumentException($"Invalid operator string {this.Operator}."),
             };
+
+            return Expression.Lambda<Func<TModel, bool>>(comparison, parameter);
         }
     }
 }
