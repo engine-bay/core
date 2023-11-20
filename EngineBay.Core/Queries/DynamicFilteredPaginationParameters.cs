4f5617b618db278c830eef4f6792a6ab033362b9
GreatMar2 <greatmar2@gmail.com>
2023-11-20 14:10:58 +0000

    Got DynamicFilteredPaginationParameters working. Old attempts are left in for this commit for future reference. Will remove in next commit.

---
+++ EngineBay.Core/Queries/DynamicFilteredPaginationParameters.cs
@@ -0,0 +1,24 @@
+﻿namespace EngineBay.Core
+{
+    public class DynamicFilteredPaginationParameters : PaginationParameters
+    {
+        public DynamicFilteredPaginationParameters(int? skip, int? limit, string? sortBy, SortOrderType? sortOrder, IList<string>? filters)
+            : base(skip, limit, sortBy, sortOrder)
+        {
+            this.Filters = new List<Filter>();
+
+            if (filters != null)
+            {
+                foreach (var filter in filters)
+                {
+                    if (!string.IsNullOrEmpty(filter))
+                    {
+                        this.Filters.Add(new Filter(filter));
+                    }
+                }
+            }
+        }
+
+        public IList<Filter> Filters { get; }
+    }
+}
