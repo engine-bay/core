ed8a0b11bfa4e4d25f9445a9a99db6a4294f27f8
windy-worm <31342528+windy-worm@users.noreply.github.com>
2023-12-06 15:14:18 +0000

    #major. User identity (#56)
    * #major. remove async methods from ICurrentIdentity interface
    
    * add HasPermission method to ICurrentIdentity
    
    * remove unnecessary interface
    
    ---------
    
    Co-authored-by: Janus Liebenberg <janus.liebenberg@entelect.co.uk>

--- EngineBay.Core/Interfaces/IQueryHandler.cs
+++ EngineBay.Core/Interfaces/IQueryHandler.cs
@@ -1,7 +1,5 @@
 namespace EngineBay.Core
 {
-    using System.Security.Claims;
-
     public interface IQueryHandler<TQueryParameters, TOutputDto>
     {
         public Task<TOutputDto> Handle(TQueryParameters query, CancellationToken cancellation);
