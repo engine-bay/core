f9ac6801612ff7ee591310dde8a51421df04c4a6
windy-worm <31342528+windy-worm@users.noreply.github.com>
2024-01-09 09:33:15 +0000

    #major: Break dependency with EF Core (#66)
    * #major: move BaseModel to persistence to break dependency with EF Core
    
    * update xunit version number
    
    ---------
    
    Co-authored-by: Janus Liebenberg <janus.liebenberg@entelect.co.uk>

--- EngineBay.Core/BaseModule.cs
+++ EngineBay.Core/BaseModule.cs
@@ -1,12 +1,9 @@
 namespace EngineBay.Core
 {
-    using System.Collections.Generic;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Routing;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Newtonsoft.Json;
 
     public abstract class BaseModule : IModule
     {
@@ -33,72 +30,6 @@ namespace EngineBay.Core
         public virtual void SeedDatabase(string seedDataPath, IServiceProvider serviceProvider)
         {
             return;
-        }
-
-        protected void LoadSeedData<TInputParameters, TOutputDto, TCommandHandler>(
-            string seedDataPath,
-            string glob,
-            IServiceProvider serviceProvider)
-            where TInputParameters : class
-            where TCommandHandler : ICommandHandler<TInputParameters, TOutputDto>
-        {
-            var commandHandler = serviceProvider.GetRequiredService<TCommandHandler>();
-
-            if (Directory.Exists(seedDataPath))
-            {
-                foreach (string filePath in Directory.EnumerateFiles(seedDataPath, glob, SearchOption.AllDirectories))
-                {
-                    List<TInputParameters>? data =
-                        JsonConvert.DeserializeObject<List<TInputParameters>>(File.ReadAllText(filePath));
-                    if (data is not null)
-                    {
-                        foreach (var entity in data)
-                        {
-                            _ = commandHandler.Handle(entity, CancellationToken.None).Result;
-                        }
-                    }
-                }
-            }
-        }
-
-        protected void LoadSeedData<TInputParameters, TOutputDto, TCommandHandler>(
-            TInputParameters[] seedData,
-            IServiceProvider serviceProvider)
-            where TInputParameters : class
-            where TCommandHandler : ICommandHandler<TInputParameters, TOutputDto>
-        {
-            ArgumentNullException.ThrowIfNull(seedData);
-
-            var commandHandler = serviceProvider.GetRequiredService<TCommandHandler>();
-
-            foreach (var entity in seedData)
-            {
-                _ = commandHandler.Handle(entity, CancellationToken.None).Result;
-            }
-        }
-
-        protected void LoadSeedData<TInputParameters, TModuleDbContext>(
-           string seedDataPath,
-           string glob,
-           IServiceProvider serviceProvider)
-           where TInputParameters : BaseModel
-           where TModuleDbContext : DbContext
-        {
-            var dbContext = serviceProvider.GetRequiredService<TModuleDbContext>();
-
-            if (Directory.Exists(seedDataPath))
-            {
-                foreach (string filePath in Directory.EnumerateFiles(seedDataPath, glob, SearchOption.AllDirectories))
-                {
-                    List<TInputParameters>? data = JsonConvert.DeserializeObject<List<TInputParameters>>(File.ReadAllText(filePath));
-
-                    if (data is not null)
-                    {
-                        dbContext.AddRange(data);
-                        dbContext.SaveChanges();
-                    }
-                }
-            }
         }
     }
 }
